65168a2eb4a2ea2e66acdc9a9344a04138cea483
Darchen-lang <samaksh114@gmail.com>
2025-10-01 18:36:48 +0530

    resume fix

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,7 @@ export const AboutSection = () => {
                             </a>
         
                             <a
-                                href="/public/samaksh-cv.pdf" target="_blank"
+                                href="/resume.pdf" target="_blank"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 Download CV
